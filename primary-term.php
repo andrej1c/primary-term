13bb9092ff1340ee7437f718b75043f20d06f430
Andrej Ciho <andrej.ciho@gmail.com>
2014-08-20 14:06:48 -0400

    Renaming 'tag' to 'term' and replacing constants with class variables with filters applied

--- primary-term.php
+++ primary-term.php
@@ -1,35 +1,38 @@
 <?php
 /* 
- * Plugin Name: Primary Tag
- * Description: WordPress plugin that allows post authors to select a primary tag.
+ * Plugin Name: Primary Term
+ * Description: WordPress plugin that allows post authors to select a primary term for a post.
  * Plugin URI: https://github.com/andrej1c/primary-term
  * Version: 1.0
  * Author: Andrej Ciho
  * Author URI: http://andrejciho.com
  */
 
-class AC_Primary_Tag
+class AC_Primary_Term
 {
-	// post meta key we will use to store the primary tag id
-	const POST_META_KEY      = '_ac_primary_tag';
-	const TAXONOMY           = 'post_tag'; // tested with 'category' and 'post_tag'
-	const TAXONOMY_NICE_NAME = 'Tag';
-	
+	static $post_meta_key;
+	static $taxonomy;
+	static $taxonomy_nice_name;
+
 	// Hook into actions and filters
 	static public function init() {
 		add_action( 'add_meta_boxes', array( __CLASS__, 'add_meta_box' ) );
 		add_action( 'save_post', array( __CLASS__, 'save' ) );
-		add_action( 'ac_primary_tag', array( __CLASS__, 'display_tag' ), 10, 1 );
+		add_action( 'ac_primary_term', array( __CLASS__, 'display_term' ), 10, 1 );
 		add_action( 'delete_term', array( __CLASS__, 'delete_term' ), 10, 3 );
-		add_filter( 'get_the_terms', array( __CLASS__, 'show_primary_tag_first' ), 10, 3 );
+		add_filter( 'get_the_terms', array( __CLASS__, 'show_primary_term_first' ), 10, 3 );
 		add_filter( 'wp_dropdown_cats', array( __CLASS__, 'append_0th_option' ), 10, 2 );
+
+		self::$post_meta_key      = apply_filters( 'ac_primary_tag_post_meta_key', '_ac_primary_term' );
+		self::$taxonomy           = apply_filters( 'ac_primary_tag_taxonomy', 'post_tag' ); // tested with 'category' and 'post_tag'
+		self::$taxonomy_nice_name = apply_filters( 'ac_primary_tag_taxonomy_nice_name', 'Tag' );
 	}
 	
 	static public function add_meta_box( $post_type ) {
 		if ( 'post' == $post_type ) {
 			add_meta_box(
-				'ac_primary_tag_meta_box' ,
-				_( 'Primary ' . self::TAXONOMY_NICE_NAME ),
+				'ac_primary_term_meta_box' ,
+				_( 'Primary ' . self::$taxonomy_nice_name ),
 				array( __CLASS__, 'render_meta_box_content' ),
 				$post_type,
 				'advanced',
@@ -45,14 +48,14 @@ class AC_Primary_Tag
 		 */
 
 		// Check if our nonce is set.
-		if ( ! isset( $_POST['ac_primary_tag_inner_custom_box_nonce'] ) ) {
+		if ( ! isset( $_POST['ac_primary_term_inner_custom_box_nonce'] ) ) {
 			return $post_id;
 		}
 
-		$nonce = $_POST['ac_primary_tag_inner_custom_box_nonce'];
+		$nonce = $_POST['ac_primary_term_inner_custom_box_nonce'];
 
 		// Verify that the nonce is valid.
-		if ( ! wp_verify_nonce( $nonce, 'ac_primary_tag_inner_custom_box' ) ) {
+		if ( ! wp_verify_nonce( $nonce, 'ac_primary_term_inner_custom_box' ) ) {
 			return $post_id;
 		}
 
@@ -70,10 +73,10 @@ class AC_Primary_Tag
 		/* OK, its safe for us to save the data now. */
 
 		// Sanitize the user input.
-		$mydata = sanitize_text_field( $_POST['ac_primary_tag_field'] );
-		$mydata = $_POST['ac_primary_tag_field'];
+		$mydata = sanitize_text_field( $_POST['ac_primary_term_field'] );
+		$mydata = $_POST['ac_primary_term_field'];
 		// Update the meta field.
-		update_post_meta( $post_id, self::POST_META_KEY, $mydata );
+		update_post_meta( $post_id, self::$post_meta_key, $mydata );
 	}
 	
 	/**
@@ -84,10 +87,10 @@ class AC_Primary_Tag
 	static public function render_meta_box_content( $post ) {
 	
 		// Add an nonce field so we can check for it later.
-		wp_nonce_field( 'ac_primary_tag_inner_custom_box', 'ac_primary_tag_inner_custom_box_nonce' );
+		wp_nonce_field( 'ac_primary_term_inner_custom_box', 'ac_primary_term_inner_custom_box_nonce' );
 
 		// Use get_post_meta to retrieve an existing value from the database.
-		$current_value = self::get_primary_tag( $post->ID );
+		$current_value = self::get_primary_term( $post->ID );
 		
 		// Check if term still exists
 		if ( ! empty( $current_value ) ) {
@@ -95,17 +98,17 @@ class AC_Primary_Tag
 
 			// Delete from this post if term no longer exists
 			if ( empty( $exists ) ) {
-				self::remove_primary_tag( $post->ID );
+				self::remove_primary_term( $post->ID );
 				$current_value = 0;
 			}
 		}
 		
 		// Display the form, using the current value.
-		echo '<label for="ac_primary_tag_field">';
-		_e( 'Primary ' . self::TAXONOMY_NICE_NAME );
+		echo '<label for="ac_primary_term_field">';
+		_e( 'Primary ' . self::$taxonomy_nice_name );
 		echo '</label> ';
 		wp_dropdown_categories( array(
-			'taxonomy'     => self::TAXONOMY,
+			'taxonomy'     => self::$taxonomy,
 			'hide_empty'   => false,
 			'hierarchical' => true,
 			'class'        => 'ac_primary_term_dropdown',
@@ -113,39 +116,39 @@ class AC_Primary_Tag
 		) );
 	}
 	
-	static public function display_tag( $show_as_link = false ) {
-		echo self::get_display_tag( $show_as_link );
+	static public function display_term( $show_as_link = false ) {
+		echo self::get_display_term( $show_as_link );
 	}
 	
-	static public function get_display_tag( $show_as_link = false ) {
+	static public function get_display_term( $show_as_link = false ) {
 		// We're assuming this is happening within a loop as it is tied to a post
 		global $post;
 		if ( empty( $post ) ) {
 			return false;
 		}
 		
-		$primary_tag_id = self::get_primary_tag( $post->ID );
+		$primary_term_id = self::get_primary_term( $post->ID );
 		
-		if ( empty( $primary_tag_id ) ) {
+		if ( empty( $primary_term_id ) ) {
 			return;
 		}
 		
-		$primary_tag = get_term( $primary_tag_id, self::TAXONOMY );
+		$primary_term = get_term( $primary_term_id, self::$taxonomy );
 		
-		if ( empty( $primary_tag ) ) {
+		if ( empty( $primary_term ) ) {
 			return;
 		}
 		
 		if ( false == $show_as_link ) {
-			return $primary_tag->name;
+			return $primary_term->name;
 		} else {
-			$permalink = get_term_link( $primary_tag, self::TAXONOMY );
-			return sprintf( '<a href="%s">%s</a>', $permalink, $primary_tag->name );
+			$permalink = get_term_link( $primary_term, self::$taxonomy );
+			return sprintf( '<a href="%s">%s</a>', $permalink, $primary_term->name );
 		}
 	}
 
 	/*
-	 * Fires after a tag is deleted so we can remove the primary tag from posts
+	 * Fires after a term is deleted so we can remove the primary term from posts
 	 * 
 	 * @param int     $term         Term ID.
 	 * @param int     $tt_id        Term taxonomy ID.
@@ -153,13 +156,13 @@ class AC_Primary_Tag
 	 *                              by the parent function. WP_Error otherwise.
 	 */
 	static public function delete_term( $term, $tt_id, $taxonomy, $deleted_term ) {
-		if ( self::TAXONOMY != $taxonomy ) {
+		if ( self::$taxonomy != $taxonomy ) {
 			return;
 		}
 
 		global $wpdb;
 		// WordPress already verified that the user has permissions to delete the term so we'll execute the query
-		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->postmeta WHERE meta_key='%s' AND meta_value=%d", self::POST_META_KEY, $term ) );
+		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->postmeta WHERE meta_key='%s' AND meta_value=%d", self::$post_meta_key, $term ) );
 	}
 	
 	/**
@@ -169,7 +172,7 @@ class AC_Primary_Tag
 	 * @return boolean|object False if term doesn exist. Term object if it does
 	 */
 	static public function get_term( $term_id ) {
-		$term = get_term_by( 'id',  (int) $term_id, self::TAXONOMY );
+		$term = get_term_by( 'id',  (int) $term_id, self::$taxonomy );
 		if ( $term ) {
 			return $term;
 		} else {
@@ -178,17 +181,17 @@ class AC_Primary_Tag
 	}
 
 	/**
-	 * Filter the list of terms attached to the given post to show the primary tag first. Useful for some breadcrumb plugins.
+	 * Filter the list of terms attached to the given post to show the primary term first. Useful for some breadcrumb plugins.
 	 *
 	 * @param array  $terms    List of attached terms.
 	 * @param int    $post_id  Post ID.
 	 * @param string $taxonomy Name of the taxonomy.
 	 */
-	static public function show_primary_tag_first( $terms, $post_id, $taxonomy ) {
-		if ( self::TAXONOMY != $taxonomy ) {
+	static public function show_primary_term_first( $terms, $post_id, $taxonomy ) {
+		if ( self::$taxonomy != $taxonomy ) {
 			return $terms;
 		}
-		$current_value = self::get_primary_tag( $post_id );
+		$current_value = self::get_primary_term( $post_id );
 
 		// if no terms, return
 		if ( empty( $terms ) ) {
@@ -236,8 +239,8 @@ class AC_Primary_Tag
 		// Append name and id to select
 		$select = $doc->getElementById( 'cat' );
 		$select = $xpath->query( "//*[@id='cat']" )->item( 0 );
-		$select->setAttribute( 'name', 'ac_primary_tag_field' );
-		$select->setAttribute( 'id',   'ac_primary_tag_field' );
+		$select->setAttribute( 'name', 'ac_primary_term_field' );
+		$select->setAttribute( 'id',   'ac_primary_term_field' );
 
 		// new node will be inserted before this node
 		$next = $xpath->query( $next_path );
@@ -260,22 +263,22 @@ class AC_Primary_Tag
 	}
 
 	/**
-	 * Get primary tag of a post
+	 * Get primary term of a post
 	 * 
 	 * @param integer $post_id 
 	 * @return mixed|null|WP_Error Term Row from database. Will return null if $term is empty.
 	 */
-	static public function get_primary_tag( $post_id ) {
-		return get_post_meta( $post_id, self::POST_META_KEY, true );
+	static public function get_primary_term( $post_id ) {
+		return get_post_meta( $post_id, self::$post_meta_key, true );
 	}
 	
 	/**
-	 * Delete primary tag reference for this post
+	 * Delete primary term reference for this post
 	 *
 	 * @param type $post_id
 	 */
-	static public function remove_primary_tag( $post_id ) {
-		delete_post_meta( $post_id, self::POST_META_KEY );
+	static public function remove_primary_term( $post_id ) {
+		delete_post_meta( $post_id, self::$post_meta_key );
 	}
 }
-add_action( 'init', 'AC_Primary_Tag::init' );
+add_action( 'init', 'AC_Primary_Term::init' );
